35d013fb1eb87a2daefad532fa0df7e0782e1ffd
GodXero <godxero.dev@gmail.com>
2025-05-10 20:35:41 +0530

    Chat system started

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,119 @@
-async function loadClientChat () {}
+let ws = null;
+const chatContent = document.getElementById('chat-content');
+const newChatForm = document.getElementById('new-chat-start-form');
+const newChatFormName = document.getElementById('new-chat-form-name');
+const newChatFormConfirmBtn = document.getElementById('new-chat-name-confirm-btn');
+
+let newChatOpenConfirmBtnListener = null;
+
+function getUUID () {
+	let uuid = localStorage.getItem('xero-chat-uuid');
+
+	if (!uuid) {
+		uuid = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+		localStorage.setItem('xero-chat-uuid', uuid);
+	}
+
+	return uuid;
+}
+
+function openNewChatForm () {
+	return new Promise((resolve) => {
+		newChatForm.classList.remove('hide');
+
+		newChatOpenConfirmBtnListener = () => {
+			const name = newChatFormName.value;
+
+			newChatForm.classList.add('hide');
+			resolve(name);
+		};
+
+		newChatFormConfirmBtn.addEventListener('click', newChatOpenConfirmBtnListener);
+	});
+}
+
+function connectToChatServer () {
+	ws = new WebSocket(`${location.protocol === 'https:' ? 'wss:' : 'ws:'}//${location.host}?uuid=${getUUID()}`);
+
+	ws.onmessage = (event) => {
+		const msg = JSON.parse(event.data);
+		console.log(msg);
+
+		if (msg.type === 'load-chat') {
+			loadClientChat(msg.chat);
+		} else if (msg.type === 'new-chat') {
+			openNewChatForm().then((name) => {
+				ws.send(JSON.stringify({
+					type: 'new-chat',
+					name
+				}));
+			});
+		} else if (msg.type === 'start-chat') {
+			console.log('start');
+		}
+	};
+
+	ws.onopen = () => {
+		console.log('Connected to server');
+	};
+
+	ws.onclose = () => {
+		console.log('Disconnected from server');
+	};
+}
+
+function disconnectFromChatServer () {
+	if (ws && ws.readyState === WebSocket.OPEN) {
+		ws.close();
+	}
+}
+
+function formatDateTime (isoString) {
+	const timeData = new Date(isoString);
+	const pad = num => String(num).padStart(2, '0');
+
+	const hours = pad(timeData.getHours());
+	const minutes = pad(timeData.getMinutes());
+	const seconds = pad(timeData.getSeconds());
+	const day = pad(timeData.getDate());
+	const month = pad(timeData.getMonth() + 1);
+	const year = timeData.getFullYear();
+
+	return {
+		time: `${hours} : ${minutes} : ${seconds}`,
+		date: `${day}/${month}/${year}`
+	};
+}
+
+function getChatBox (message, sender = null) {
+	const { time, date } = formatDateTime(message.time);
+
+	return `<div class="chat-box ${sender == null ? '' : 'client'}">
+		<div class="time">
+			<div>${time}</div>
+			<div>${date}</div>
+		</div>
+		<div class="msg">${message.message}</div>
+		<div class="sender">${sender ? sender : 'Sathish Shan'}</div>
+	</div>`;
+}
+
+async function loadClientChat (chat) {
+	chatContent.innerHTML = '';
+
+	const messages = chat.chat.sort((a, b) => new Date(a.time) - new Date(b.time));
+
+	messages.forEach(message => {
+		chatContent.innerHTML += getChatBox(message, message.fromClient === 1 ? chat.client : null);
+	});
+}
+
+document.getElementById('direct-message-btn').addEventListener('click', () => {
+	connectToChatServer();
+});
+
+document.getElementById('chat-close-btn').addEventListener('click', () => {
+	disconnectFromChatServer();
+});
+
+connectToChatServer();
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,19 @@ import express, { NextFunction, Request, Response } from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
 import mysql from 'mysql2';
+import http, { IncomingMessage } from 'http';
+import WebSocket, { WebSocketServer } from 'ws';
+import { parse } from 'url';
 
 dotenv.config();
+
+class DBError extends Error {
+	constructor (message: string) {
+		super(message);
+
+		this.name = 'DBError';
+	}
+}
 
 const dbConnection = mysql.createConnection({
 	host: process.env.DB_HOST,
@@ -24,6 +35,9 @@ dbConnection.connect((err) => {
 const app = express();
 const port = process.env.PORT || 5500;
 
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
 app.use('/site', express.static(path.join(__dirname, '../public')));
 app.use('/admin', (req: Request, res: Response, next: NextFunction) => {
 	const allowedIps = ['127.0.0.1', '::1', '::ffff:127.0.0.1'];
@@ -35,6 +49,135 @@ app.use('/admin', (req: Request, res: Response, next: NextFunction) => {
 	}
 }, express.static(path.join(__dirname, '../admin')));
 
-app.listen(port, () => {
-	console.log(`Server is running at http://localhost:${port}`);
+wss.on('connection', (ws: WebSocket, req: IncomingMessage) => {
+	const query = parse(req.url || '', true).query;
+	const deviceId = query.uuid as string;
+
+	console.log(`Client connected: ${deviceId}`);
+
+	loadChatForDevice(deviceId).then(chatData => {
+		if (chatData) {
+			ws.send(JSON.stringify({
+				type: 'load-chat',
+				chat: chatData
+			}));
+		} else {
+			ws.send(JSON.stringify({
+				type: 'new-chat'
+			}));
+		}
+	}).catch(error => console.error(error));
+
+	ws.on('message', (message) => {
+		const msg = JSON.parse(message.toString());
+		console.log('Received:', msg);
+
+		if (msg.type === 'new-chat') {
+			addNewClient(msg.name, deviceId).then(() => {
+				ws.send(JSON.stringify({
+					type: 'start-chat'
+				}));
+			}).catch((error) => console.error(error));
+		}
+	});
+
+	ws.on('close', () => console.log(`Client disconnected: ${deviceId}`));
 });
+
+server.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
+
+function getClientFromDeviceID (deviceId: string): Promise<any> {
+	return new Promise((resolve, reject) => {
+		const queryClient = 'SELECT id, name FROM client_name WHERE device_id = ?';
+
+		dbConnection.query(queryClient, [deviceId], (err, clientRows) => {
+			if (err) {
+				reject(err.message);
+				return;
+			}
+
+			if ((clientRows as any[]).length === 0) {
+				// No client found for device ID
+				resolve(null);
+				return;
+			}
+
+			// Found client
+			resolve(clientRows[0]);
+		});
+	});
+}
+
+function getChatForClient (clientID: number): Promise<any> {
+	return new Promise((resolve, reject) => {
+		const chat = [];
+		const queryChat = 'SELECT from_client, message, time FROM chat WHERE client = ?';
+
+		dbConnection.query(queryChat, [clientID], (err, chatRows) => {
+			if (err) {
+				reject(err.message);
+				return;
+			}
+
+			(chatRows as any[]).forEach((msg: any) => {
+				chat.push({
+					message: msg.message,
+					time: msg.time,
+					fromClient: msg.from_client
+				});
+			});
+
+			resolve(chat);
+		});
+	});
+}
+
+function loadChatForDevice (deviceId: string): Promise<any> {
+	return new Promise(async (resolve, reject) => {
+		let client = null;
+
+		try {
+			client = await getClientFromDeviceID(deviceId);
+		} catch (error) {
+			reject(error);
+			return;
+		}
+
+		if (client == null) {
+			resolve(null);
+			return;
+		}
+
+		let chat = null;
+
+		try {
+			chat = await getChatForClient(client.id);
+		} catch (error) {
+			reject(error);
+			return;
+		}
+
+		resolve({
+			client: client.name,
+			chat
+		});
+	});
+}
+
+function addNewClient (name: string, deviceId: string) {
+	return new Promise((resolve, reject) => {
+		const queryInsertClient = 'INSERT INTO client_name (name, device_id) VALUES (?, ?)';
+
+		dbConnection.query(queryInsertClient, [name, deviceId], (insertErr, result) => {
+			if (insertErr) {
+				reject(new DBError('Error inserting new client: ' + insertErr.message));
+				return;
+			}
+
+			const insertId = (result as mysql.ResultSetHeader).insertId;
+
+			console.log(`New client added with ID: ${insertId}`);
+			resolve(null);
+		});
+	});
+}
